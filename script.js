fda604eebd73ec7fd4870fddd036c298b7db15d8
Dacio Azevedo <dacioherazan2@gmail.com>
2024-07-08 21:41:47 -0300

    Added user input and small change on the function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let container = document.querySelector("#container");
 
-for (let i = 1; i <= 256; i++) {
-    let squares = document.createElement("div");
-    squares.setAttribute("class", "squares");
-    container.appendChild(squares);
+function createGrade(quantity) {
+
+    let gradeSize = quantity();
+    for (let i = 1; i <= gradeSize; i++) {
+        let squares = document.createElement("div");
+        squares.setAttribute("class", "squares");
+        container.appendChild(squares);
+    }
 }
+
+let size = function() {
+    let size = prompt("Enter the number of squares: ");
+    return +size;   
+}
+
+createGrade(size);
